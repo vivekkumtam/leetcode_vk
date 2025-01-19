093b4a9fcf1e150af5187e7987c45cf483be083c
Vivek Vardhan Reddy Kumtam <153547732+vivekkumtam@users.noreply.github.com>
2025-01-18 19:36:04 -0600

    Time: 7 ms (57.85%) | Memory: 19.4 MB (11.56%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        ans = None
-        count = 0
-
-        for num in nums:
-            if count == 0:
-                ans = num
-            count += (1 if num == ans else -1)
-
-        return ans
+        count = Counter(nums)
+        for num, freq in count.items():
+            if freq > len(nums) // 2:
+                return num
+        return None
